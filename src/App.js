ba8a6b66e1c415463c628b71a7f9fed0b022ad52
Manu <manuel.chavez@accenture.com>
2022-08-12 08:54:19 -0500

    Using components to view interface

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import { BiArchive } from 'react-icons/bi'
+import { BiCalendar, BiTrash } from 'react-icons/bi'
+import Search from './components/search'
+import AddAppointment from './components/addAppointments'
+import appointmentList from './data.json'
+import AppointmentInfo from './components/appointmentInfo'
+
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className='text-5xl'>
-        <BiArchive className='inline-block text-red-400 align-top'/>
+      <h1 className='text-5xl mb-3'>
+        <BiCalendar className='inline-block text-red-400 align-top'/>
         Your Appointments
       </h1>
+      <AddAppointment />
+      <Search />
+
+      <ul className='divide-y divide-gray-200'>
+        {
+          appointmentList.map(appointment => (
+              <AppointmentInfo key={appointment.Id} appointment={appointment}/>
+          ))
+        }
+      </ul>
     </div>
   );
 }
